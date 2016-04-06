e522b579a09606e37d890e18769c62f1db805dd6
Kristen Gillette <kristenmgillette@gmail.com>
2016-04-06 17:48:55 -0400

    Message submitting system

--- js/alert.js
+++ js/alert.js
@@ -1,12 +1,12 @@
-var alert = 0;
+var alertCount = 0;
 
 $( ".main-content" ).prepend( "<div class='alert'><span class='highlight'>Alert</span> There are currently turtles trying to cross the road.<span class='exit'>X</span></div>");
 
 $("#line").prepend("<img id='alert-dot' src='icons/alert-dot.png' />");
 
 $("#alert").click(function(){
-	if (alert === 0) {
-		alert += 1; 
+	if (alertCount === 0) {
+		alertCount += 1; 
 	$( ".main-content" ).prepend( "<div class='alert'><span class='highlight'>Alert</span> There are currently 5 turtles trying to cross the road.<span class='exit'>X</span></div>");
 	$( ".main-content" ).prepend( "<div class='alert'><span class='highlight'>Alert</span> There are currently 10 turtles trying to cross the road.<span class='exit'>X</span></div>");
 	$( "#alert-dot" ).remove();
