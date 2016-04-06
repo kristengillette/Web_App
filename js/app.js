cb5348fa4352df1c79c8869105421371109f5658
Kristen Gillette <kristenmgillette@gmail.com>
2016-04-06 17:49:42 -0400

    Adding app.js file

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(".nav li").click(function(){
+    $(".nav li").removeAttr('id', 'menu-page-selected');
+    $(this).attr('id', 'menu-page-selected');
+})
+
+$("form").submit(function(){
+    if (!$(".user-message").val()) {
+    $("#warning").text("Please enter a message").show();
+} else {
+	$("#warning").empty()
+	$("#warning").text("Thanks for sending your message!");
+}
+});
+
